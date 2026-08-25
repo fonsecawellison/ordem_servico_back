af2ab5d4b115a8ed62376e22e00f81c20676e736
fonsecawellison <fonsecawellison13@gmail.com>
2026-08-24 20:20:42 -0400

    Commit changes
    Comment out the CORS options middleware.

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('*', cors());
+//app.options('*', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
